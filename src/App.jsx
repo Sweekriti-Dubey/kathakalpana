d9d5dbb212616148f205fbcf860ce2a694361af4
Sweekriti <sweekritidubey1@gmail.com>
2025-03-26 02:21:56 +0530

    dh

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import { Book, Star, Users, Info, Volume2 } from 'lucide-react';
-import mouseImage from './images/mouse.png'; // Import the mouse image
+import mouseImage from '../images/mouse.png'; // Import the mouse image
 
 
 function App() {
